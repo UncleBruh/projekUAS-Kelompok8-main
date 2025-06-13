ac6a877c631d8dbcf8c00a8a2145e74e1826b57c
Nelson <74392133+UncleBruh@users.noreply.github.com>
2025-06-13 21:54:39 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\BarangKeluarController;
+use App\Http\Controllers\BarangMasukController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::resource('/barang', BarangController::class);
+Route::resource('/barangmasuk', BarangMasukController ::class);
+Route::resource('/barangkeluar', BarangKeluarController::class);
+Route::resource('/dashboard', DashboardController::class);
+Route::get('/tambahbarang', [BarangController::class, 'create'])->name('barang.create');
